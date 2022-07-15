b680002e6754e9a8413f593a79454cdf6ad29345
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-07-15 22:08:03 +1200

    data analysis in progress

--- client/main.py
+++ client/main.py
@@ -15,16 +15,27 @@ no_duplicate_list_of_names = list(set(list_of_names))
 column_1 = [ele.split()[0] for ele in no_duplicate_list_of_names]
 column_1 = sorted(column_1)
 
-# prepare other columns
-total_checkins = skodel_df['name'].value_counts()
+
+total_checkins = skodel_df.groupby('name')['name'].value_counts()
 total_mood_score = skodel_df.groupby('name')['mood'].sum()
+# average mood score per check in
 column_4 = (total_mood_score / total_checkins ).tolist()
 column_2 = total_checkins.tolist()
 column_3 = total_mood_score.tolist()
 
-# create df-> excel
-skodel_summary_data_df = pd.DataFrame(list(zip(column_1, column_2, column_3, column_4)),
-             columns =['name', 'total_checkins', 'total_mood_score', 'mood_mean_score'])
+# average words per check in
+skodel_df['total_words'] = skodel_df['third_question_response'].str.count(' ') + 1
+# class average
+total_words_class = skodel_df['total_words'].sum()
+total_checkins_class = len(skodel_df)
+class_average_words = int(total_words_class / total_checkins_class)
+# per student
+total_words_student = skodel_df.groupby('name')['total_words'].sum()
+column_5 = (total_words_student / total_checkins ).tolist()
 
-skodel_summary_data_df.to_excel('skodel_summary_data.xlsx' )
+# create df-> excel
+# skodel_summary_data_df = pd.DataFrame(list(zip(column_1, column_2, column_3, column_4, total_class_mean)),
+#              columns =['name', 'total_checkins', 'total_mood_score', 'mood_mean_score', 'total_class_mean'])
+
+# skodel_summary_data_df.to_excel('skodel_summary_data.xlsx')
 
