458a0117352de5ab4d3acc8a4b6052a54824cc8c
rpet064 <93119631+rpet064@users.noreply.github.com>
2022-07-19 10:05:07 +1200

    clean tags and response columns

--- client/main.py
+++ client/main.py
@@ -6,22 +6,22 @@ csv_url = 'skodel_data.csv'
 skodel_df = pd.read_csv(csv_url, encoding='latin1')
 skodel_df.fillna('', inplace= True)
 
-# whole class data
-total_class_mean = skodel_df['mood'].mean()
+# # whole class mean mood
+# column_8 = skodel_df['mood'].mean()
 
-# prepare names, no duplicate, alphabetical
-list_of_names = skodel_df['name'].tolist()
-no_duplicate_list_of_names = list(set(list_of_names))
-column_1 = [ele.split()[0] for ele in no_duplicate_list_of_names]
-column_1 = sorted(column_1)
+# # prepare names, no duplicate, alphabetical
+# list_of_names = skodel_df['name'].tolist()
+# no_duplicate_list_of_names = list(set(list_of_names))
+# column_1 = [ele.split()[0] for ele in no_duplicate_list_of_names]
+# column_1 = sorted(column_1)
 
-
-total_checkins = skodel_df.groupby('name')['name'].value_counts()
-total_mood_score = skodel_df.groupby('name')['mood'].sum()
-# average mood score per check in
-column_4 = (total_mood_score / total_checkins ).tolist()
-column_2 = total_checkins.tolist()
-column_3 = total_mood_score.tolist()
+# # total checkins & total mood score
+# total_checkins = skodel_df.groupby('name')['name'].value_counts()
+# total_mood_score = skodel_df.groupby('name')['mood'].sum()
+# # average mood score per check in
+# column_4 = (total_mood_score / total_checkins ).tolist()
+# column_2 = total_checkins.tolist()
+# column_3 = total_mood_score.tolist()
 
 # average words per check in
 skodel_df['total_words'] = skodel_df['third_question_response'].str.count(' ') + 1
@@ -33,9 +33,24 @@ class_average_words = int(total_words_class / total_checkins_class)
 total_words_student = skodel_df.groupby('name')['total_words'].sum()
 column_5 = (total_words_student / total_checkins ).tolist()
 
-# create df-> excel
-# skodel_summary_data_df = pd.DataFrame(list(zip(column_1, column_2, column_3, column_4, total_class_mean)),
-#              columns =['name', 'total_checkins', 'total_mood_score', 'mood_mean_score', 'total_class_mean'])
+# # extract tags
+# tags_list = skodel_df['tags'].tolist()
+# column_6 = list(filter(None, tags_list))
 
-# skodel_summary_data_df.to_excel('skodel_summary_data.xlsx')
+# # extract responses
+# response_list = skodel_df['third_question_response'].tolist()
+# response_list = [resp.replace('\r', '').replace('.', '').replace('-', '').replace('?', '').replace('_\x98_20220515 20:00:09605585+00:00', '') for resp in response_list]
+# norm_response_list = list(filter(None, response_list))
+# column_7 = " ".join(norm_response_list).split()
+# column_7.remove('&s').remove('1')
+
+# # create df-> excel
+# student_summary_data_df = pd.DataFrame(list(zip(column_1, column_2, column_3, column_4, column_5)),
+#              columns =['name', 'total_checkins', 'total_mood_score', 'mood_mean_score'])
+
+# class_summary_data_df = pd.DataFrame(list(zip(column_5, column_6, column_7, column_8)),
+#              columns =['tags', 'responses', 'total_class_mean'])
+
+# student_summary_data_df.to_excel('student_summary_data.xlsx')
+# class_summary_data_df.to_excel('class_summary_data.xlsx')
 
